d9454aa091ff6c42d95a6d7f07b3e6cb1c1b1c8b
minuk0428 <minha8000@gmail.com>
2023-09-24 16:28:47 +0900

    1541

--- 1463.py
+++ 1463.py
@@ -1,4 +1,18 @@
-a = 2
-list = [0] * (a+1)
+import sys
 
-print(list)
+a = int(sys.stdin.readline())
+dp = [0]*(10**6 + 1)
+ 
+dp[1] = 0
+dp[2] = 1
+dp[3] = 1
+for i in range(4,a+1):
+    dp[i] = dp[i-1] + 1
+   
+    if i % 2 == 0:
+        dp[i] = min(dp[i//2] + 1, dp[i])
+       
+    if i % 3 == 0:
+        dp[i] = min(dp[i//3] + 1, dp[i])
+
+print(dp[a])
---
+++ 1541.py
@@ -0,0 +1,12 @@
+import sys
+
+m = input().split('-')
+s = 0
+for i in m[0].split('+'):
+    s+= int(i)
+
+for i in m[1:]:
+    for j in i.split('+'):
+        s -= int(j)
+ 
+print(s)
