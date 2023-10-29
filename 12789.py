54174124ec58d598abf7a927211bf30bdfe3a540
rumongdaromi <coderromi@gmail.com>
2023-10-29 23:17:07 +0900

    12789

---
+++ 12789.py
@@ -0,0 +1,37 @@
+import sys
+import copy
+input = sys.stdin.readline
+#스택 큐 둘 다 사용 => 줄서는곳은 FIFO => 큐 , 한명씩설수있는공간은 LIFO => 스택
+from collections import deque
+N = int(input())
+q = list(map(int,input().split()))
+나 = 1
+
+줄서는곳 = deque()ㄴ
+for i in q:
+    줄서는곳.append(i)
+간식받는곳 = []
+
+한명씩설수있는공간 = deque()
+
+# print(줄서는곳.popleft()) # ==5
+while 줄서는곳:
+    if 줄서는곳 and 줄서는곳[0] == 나:
+        줄서는곳.popleft()
+        나 += 1
+    else:
+        한명씩설수있는공간.append(줄서는곳.popleft())
+    
+    while 한명씩설수있는공간 and 한명씩설수있는공간[-1] == 나:
+        한명씩설수있는공간.pop()
+        나 += 1
+
+if 한명씩설수있는공간:
+    print('Sad')
+else:
+    print('Nice')
+
+
+ 
+
+
